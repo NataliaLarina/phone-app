fabc1c946f33c5811a7008b6ab3bce255611a31a
Natalia Larina <koshka_208@yahoo.com>
2017-02-27 13:44:29 -0600

    dialpad change

--- scripts/app.js
+++ scripts/app.js
@@ -1,10 +1,10 @@
-// $(document).ready(function () {
-//
-//     $('.btn-xl').click(function () {
-//         var num = $(this);
-//         var text = $.trim(num.find('.txt').clone().children().remove().end().text());
-//         var telNumber = $('#telNumber');
-//         $(telNumber).val(telNumber.val() + text);
-//     });
-//
-// });
+$(document).ready(function () {
+
+    $('.btn-xl').click(function () {
+        var num = $(this);
+        var text = $.trim(num.find('.num').clone().children().remove().end().text());
+        var telNumber = $('#telNumber');
+        $(telNumber).val(telNumber.val() + text);
+    });
+
+});
