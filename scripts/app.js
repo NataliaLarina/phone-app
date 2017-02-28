277881d21014e8e2bfc565172481433eb8d5a880
Natalia Larina <koshka_208@yahoo.com>
2017-02-28 16:58:06 -0600

    changes2

--- scripts/app.js
+++ scripts/app.js
@@ -8,3 +8,16 @@ $(document).ready(function () {
     });
 
 });
+
+function limitKeypress(event, value, maxLength) {
+    if (value != undefined && value.toString().length >= maxLength) {
+        event.preventDefault();
+    }
+}
+
+
+function showMessage(){
+        var phone = document.getElementById("telNumber").value;
+        document.write('<h1 style="color:purple; text-align:center; font-size: 100px; margin-top:50px;">' + 'You dialed:' + '</h1>')
+        document.write('<h1 style="color:black; text-align:center; font-size: 100px;">' + phone + '</h1>');
+        }
